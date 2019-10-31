fd2228d693b621e41719d5b1f6735eef713060bc
Eliott Frilet <frilet.eliott@gmail.com>
2019-10-31 16:26:39 -0500

    Initialize react filebase

---
+++ client/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return(
+      <div className="App">
+        <h1> Hello fellow mortals! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
