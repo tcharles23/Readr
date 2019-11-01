d3556e50dff103afb9ad80e03acac2cd67e7732c
Eliott Frilet <frilet.eliott@gmail.com>
2019-11-01 18:39:36 -0500

    Testing port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
