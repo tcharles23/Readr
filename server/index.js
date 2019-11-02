11e6acfa6f7b259b932bfee2fb523c4245b6bbd6
Eliott Frilet <frilet.eliott@gmail.com>
2019-11-02 16:11:25 -0500

    Add route for authentication

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const authRoutes = require('./auth-routes');
+
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.json());
+
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port :${PORT}!`);
