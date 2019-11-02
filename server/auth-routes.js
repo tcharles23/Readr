2ca287f3589fd780d74bf74b059a8337c6c1db38
Eliott Frilet <frilet.eliott@gmail.com>
2019-11-02 18:07:24 -0500

    Add /google/redirect route for successful google authentication

--- server/auth-routes.js
+++ server/auth-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 // auth login
 router.get('/login', (req, res) => {
@@ -12,9 +13,16 @@ router.get('/logout', (req, res) => {
 });
 
 // auth with google
-router.get('/google', (req, res) => {
-  // handle with passport
-  res.send('logging in with google');
+router.get('/google', passport.authenticate('google', {
+  // Sends user to consent screen
+  scope: ['profile'],
+}));
+
+// callback route for google to redirect to
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+  // access current user through req.user
+  // redirect to post login page
+  res.send('you reached the callback URI');
 });
 
 module.exports = router;
