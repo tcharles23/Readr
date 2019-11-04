66fc188c895df951171ca2e85b86df4f6dd861d6
Amber <amber.j.nola@gmail.com>
2019-11-04 11:37:47 -0600

    added axios as dependency

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -20,6 +20,10 @@ class App extends React.Component {
     this.setState({ isLoggedIn: true });
   }
 
+  getBookSuggestions() {
+    
+  }
+
   render() {
     const { isLoggedIn } = this.state;
     return (
