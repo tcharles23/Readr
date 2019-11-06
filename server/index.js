05f972d453e52126f8c09f9f7b8be9b1f6e938f5
Abel Terefe <abelterefe98@gmail.com>
2019-11-05 20:08:46 -0600

    server/index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const models = require('../sequelize/models/index');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
@@ -9,6 +10,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-  console.log(`Listening on port :${PORT}!`);
+// sync () will create all tables to the database if they don't exist in the database
+models.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Listening on port :${PORT}!`);
+  });
 });
