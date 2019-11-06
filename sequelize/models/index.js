87aca779e7ab102479c68aade5b6ba1e018baa09
Abel Terefe <abelterefe98@gmail.com>
2019-11-06 15:38:46 -0600

    successfully added tables to the databases

--- sequelize/models/index.js
+++ sequelize/models/index.js
@@ -1,37 +1,116 @@
 const Sequelize = require('sequelize');
-// const fs = require('fs');
-// const path = require('path');
-const { user } = require('./userLogin');
-const { books } = require('./book');
-const { readList } = require('./ReadList');
+// const { user } = require('./user');
+// const { books } = require('./book');
+// const { readList } = require('./ReadList');
+// const { followList } = require('./followList');
+// const { db } = require('../db');
 require('dotenv').config();
 
-const sequelize = new Sequelize('readrs', 'cheap_readers', 'READRSARELEADERS2020', { dialect: 'postgres' });
+
+const {
+  DATABASE,
+  HOST,
+  DB_PORT,
+} = process.env;
+
+const db = new Sequelize(DATABASE, "macbook", "Terefe",
+  {
+    host: HOST,
+    port: DB_PORT,
+    dialect: 'postgres',
+    logging: false,
+  });
 
 
-const models = {
-  user,
-  books,
-  readList,
-};
-
-// fs
-//   .readdirSync(__dirname)
-//   .filter((file) => {
-//     return (file.indexOf('.') !== 0) && (file !== 'index.js');
-//   })
-//   .forEach((file) => {
-//     const model = sequelize.import(path.join(__dirname, file));
-//     models[model.name] = model;
-//   });
-
-Object.keys(models).forEach((modelName) => {
-  if (models[modelName].associate) {
-    models[modelName].associate(models);
-  }
+// creating the table for the books api informations
+const books = db.define("books", {
+  ISBN: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  Author: Sequelize.STRING,
+  Desc: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  cover: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  url: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
 });
 
-module.exports.sequelize = sequelize;
-module.Sequelize = Sequelize;
+// creating the fields on the table
+const followList = db.define("followList", {
+  id: {
+    type: Sequelize.INTEGER,
+    // `references: {
+    //   model: 'user',
+    //   key: 'id',
+    // },`
+    primaryKey: true,
+  },
+  followers: {
+    type: Sequelize.INTEGER,
+    // references: {
+    //   model: 'user',
+    //   key: 'id',
+    // },
+  },
+});
 
-module.exports.models = models;
+// creating the table for the user
+const user = db.define("user", {
+  username: Sequelize.STRING,
+  googleId: Sequelize.STRING,
+});
+
+// creating the fields on the table
+const readList = db.define("readList", {
+  boolean: Sequelize.BOOLEAN,
+  id: {
+    type: Sequelize.INTEGER,
+    // references: {
+    //   model: 'user',
+    //   key: 'id',
+    // },
+    primaryKey: true,
+  },
+  isbn: {
+    type: Sequelize.INTEGER,
+    // references: {
+    //   model: 'books',
+    //   key: 'isbn',
+    // },
+    unique: true,
+  },
+});
+
+db.sync().then(() => {
+  console.log('connected to database');
+})
+
+
+// module.exports.db = db;
+// module.exports.user = user;
+// module.exports.books = books;
+// module.exports.readList = readList;
+// module.exports.followList = followList;
+// const models = {
+//   user,
+//   books,
+//   readList,
+//   followList,
+// };
+
+// models.user.sync();
+// models.books.sync();
+// models.readList.sync();
+// models.followList.sync();
