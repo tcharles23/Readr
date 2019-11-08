1d9d5af6b4dce51fc5380c0054e0beae65b02e6c
Amber <amber.j.nola@gmail.com>
2019-11-07 22:05:19 -0600

    added landing page, re-did the routes so that it navigates more smoothly upon logging in

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,30 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './components/App.jsx';
-// import Logout from './components/Logout';
-import SuggestionView from './components/SuggestionView.jsx';
-import BookListView from './components/BookListView.jsx';
-import ReaderView from './components/ReaderView.jsx';
-import Following from './components/FollowingView.jsx';
-import NotFound from './components/NotFound.jsx';
 
 
 ReactDOM.render(
   // creates URLs with the following format: http://localhost:3000/route/subroute
   (
     <BrowserRouter>
-      {/* a router component can only have one child */}
-      <div>
-        <Switch>
-          {/* // this is our default route */}
-          <Route exact path="/" component={App} />
-          <Route exact path="/following" component={Following} />
-          <Route exact path="/toread" component={BookListView} />
-          <Route exact path="/readnow" component={ReaderView} />
-          {/* // if noroute exists */}
-          <Route component={NotFound} />
-        </Switch>
-      </div>
+      <App />
     </BrowserRouter>
   ), document.getElementById('root'),
 );
