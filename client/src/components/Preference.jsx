58b5b51b919fc0728ee063a110accdd68411bcf6
Brandon Theard <btheard1@gmail.com>
2019-11-12 16:16:02 -0600

    added separate like and dislike button

--- client/src/components/Preference.jsx
+++ client/src/components/Preference.jsx
@@ -1,32 +1,27 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default class LikeButton extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      liked: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
-  handleClick() {
-    this.setState({
-      liked: !this.state.liked,
-    });
-  }
-
-  render() {
-    const text = this.state.liked ? 'liked' : 'disliked';
-    const label = this.state.liked ? 'Unlike' : 'Like';
-    return (
-      <div className="customContainer">
-        <button type="button" className="btn btn-primary" onClick={this.handleClick}>
-          {label}
-        </button>
-        <p>
-          you {text} this. Click to toggle.
-        </p>
+function LikeButton(props) {
+  const classes = useStyles();
+  return (
+    <div>
+      {/* centers the "not interested" and "interested" buttons */}
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Button variant="contained" size="large" color="primary" className={classes.button} onClick={() => props.handleYesClick()}>
+          Like
+        </Button>
+        <Button variant="contained" size="large" color="default" className={classes.button} onClick={() => props.handleNoClick()}>
+        Dislike
+        </Button>
       </div>
-    );
-  }
+    </div>
+  );
 }
+export default LikeButton;
