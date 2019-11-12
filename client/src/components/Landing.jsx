6cc23883b006b1bbabdc57842b7284eb0d38a49c
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-11-11 23:48:37 -0600

    Working like/dislike slider

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import { Typography, Box } from '@material-ui/core';
+import { Box } from '@material-ui/core';
+
+import Deck from './LandingTinder.jsx';
 
 
 function Landing() {
   return (
-    <div>
-      <Box m={1} mx="auto">
-        <Typography variant="body1" align="center">
-         To get started, click the Explore Books link and we&apos;ll do the rest!
-        </Typography>
-      </Box>
-    </div>
+    <Box display="flex" justifyContent="center" m={1} p={1} bgcolor="background.paper">
+      <Deck style={{ textAlign: 'center' }}>
+        <h2>Cool</h2>
+        <h2>Cool</h2>
+        <h2>Cool</h2>
+      </Deck>
+    </Box>
   );
 }
 
 export default Landing;
+
+
+// old landing code
+{/* <Box m={1} mx="auto">
+  <Typography variant="body1" align="center">
+    To get started, click the Explore Books link and we&apos;ll do the rest!
+        </Typography>
+</Box> */}
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,13 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
     ],
   },
 };
