6ba7759ae67e5d9209997393581cb6efdd348d95
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-11-13 21:35:46 -0600

    fix navbar and toast message

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable spaced-comment */
 /* This is the header or top bar component.  It is pretty static.
  * Includes navigatin.buttons- App logo Title  2..button to "To-Read" list,
  * 3. "Explore Books" (new book suggestion), 4. Logout button.
@@ -9,7 +10,9 @@ import {
   Toolbar,
   makeStyles,
   Button,
+  IconButton,
 } from '@material-ui/core';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 import { Link } from 'react-router-dom';
 import WelcomeToast from './SnackBar.jsx';
 
@@ -19,6 +22,21 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(1),
     color: 'white',
   },
+  rightToolbar: {
+    marginLeft: 'auto',
+    marginRight: 0,
+  },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    color: 'white',
+    textDecoration: 'none',
+  },
 }));
 
 const NavBar = (props) => {
@@ -26,36 +44,64 @@ const NavBar = (props) => {
   const { username, id } = props.user;
   const firstName = username.split(' ')[0];
   return (
-    <div>
-      <AppBar color="primary" position="fixed">
+    <div className={classes.root}>
+      <WelcomeToast message={`Hi ${username}!`} />
+      <AppBar position="fixed">
         <Toolbar>
-          <Typography variant="h4" align="left">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            component={Link}
+            to="/"
+          >
+            <MenuBookIcon />
+          </IconButton>
+          <Typography
+            variant="h4"
+            className={classes.title}
+            component={Link}
+            to="/suggestion"
+          >
             Readr 2.0
           </Typography>
-          <Typography align="right">
-            <Button size="large" component={Link} to="/suggestion" className={classes.button}>
-              Explore Books
-            </Button>
-            <Button size="large" component={Link} to="/booklist" className={classes.button}>
-              To-Read
-            </Button>
-            <Button size="large" component={Link} to="/following" className={classes.button}>
-              Following
-            </Button>
-            {/* This directs to passport auth logout */}
-            <Button
-              size="large"
-              href="/auth/logout"
-              className={classes.button}
-            >
-              Logout
-            </Button>
-          </Typography>
+          <Button
+            size="large"
+            component={Link}
+            to="/suggestion"
+            className={classes.button}
+          >
+            Explore Books
+          </Button>
+          <Button
+            size="large"
+            component={Link}
+            to="/booklist"
+            className={classes.button}
+          >
+            To-Read
+          </Button>
+          <Button
+            size="large"
+            component={Link}
+            to="/following"
+            className={classes.button}
+          >
+            Following
+          </Button>
+          <Button
+            size="large"
+            href="/auth/logout"
+            className={classes.button}
+          >
+            Logout
+          </Button>
         </Toolbar>
       </AppBar>
-      <WelcomeToast message={`Welcome, ${firstName}!`} />
     </div>
   );
 };
 
 export default NavBar;
+
