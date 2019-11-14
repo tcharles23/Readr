3d664cf63aec11d4e13227edce3c10480c27fb26
Titus Charles <tcharles23.tc@gmail.com>
2019-11-14 09:31:20 -0600

    take out comments

--- server/suggestion.js
+++ server/suggestion.js
@@ -21,7 +21,6 @@ const getInfo = (title, author) => axios.get('https://www.googleapis.com/books/v
   },
 })
   .then((bookData) => {
-    // console.log(bookData.data.items[0].saleInfo, 'salessss');
     const bookInfo = {};
     bookInfo.isbn = bookData.data.items[0].volumeInfo.industryIdentifiers[0].identifier || null;
     bookInfo.description = bookData.data.items[0].volumeInfo.description || null;
